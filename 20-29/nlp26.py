773f8860d4677249d068e813758553ce4e618000
emergent <yosikawa@gmail.com>
2019-05-06 00:39:01 +0900

    solve 25-27

---
+++ 20-29/nlp26.py
@@ -0,0 +1,19 @@
+from nlp20 import get_england
+import re
+
+str = get_england()
+lines = str.split('\n')[2:59]
+
+p = re.compile(r'^\|(.+)\s+=\s+(.+)$')
+kiso = {}
+lastkey = ''
+for l in lines:
+    m = p.search(l)
+    if m is not None:
+        lastkey = m.group(1)
+        kiso[lastkey] = m.group(2).replace(r"'", '')
+    else:
+        kiso[lastkey] = kiso[lastkey] + '\n' + l
+
+for k, v in kiso.items():
+    print(k, v)
